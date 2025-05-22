79aa93af352e1aa7f09349ccbbcfc94b9b07b2f9
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2025-05-21 23:51:43 -0300

    Adicionar funcionalidades para consumo e exibição de dados de séries e temporadas

--- src/main/java/br/com/alura/screenmach/ScreenmachApplication.java
+++ src/main/java/br/com/alura/screenmach/ScreenmachApplication.java
@@ -1,28 +1,23 @@
 package br.com.alura.screenmach;
 
-import br.com.alura.screenmach.model.DadosSerie;
-import br.com.alura.screenmach.service.ConsumoAPI;
-import br.com.alura.screenmach.service.ConverteDados;
+import br.com.alura.screenmach.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Define que esta é a classe principal de uma aplicação Spring Boot
 @SpringBootApplication
 public class ScreenmachApplication implements CommandLineRunner {
 
+	// Metodo principal que inicia a aplicação Spring Boot
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmachApplication.class, args);
 	}
 
+	// Metodo executado quando a aplicação inicia (interface CommandLineRunner)
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9edc4905");
-		//System.out.println(json);
-		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
