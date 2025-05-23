be1e6e1bb014580f95e1e447833f4c5244d95971
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2025-05-23 17:18:28 -0300

    Corrigir parsing de datas inválidas no construtor da classe Episodio
    - Adicionado tratamento para strings inválidas como "N/A" ou null no campo de dataLancamento.
    - Incluído bloco try-catch no construtor da classe Episodio para lidar com exceções DateTimeParseException.
    - Ajustado o fluxo da classe Principal para garantir que os episódios com datas inválidas sejam manipulados corretamente.
    - Essas alterações garantem robustez ao código e evitam falhas ao processar dados vindos de fontes externas.

--- src/main/java/br/com/alura/screenmach/principal/Principal.java
+++ src/main/java/br/com/alura/screenmach/principal/Principal.java
@@ -1,13 +1,16 @@
 package br.com.alura.screenmach.principal;
 
+import br.com.alura.screenmach.model.DadosEpisodio;
 import br.com.alura.screenmach.model.DadosSerie;
 import br.com.alura.screenmach.model.DadosTemporada;
+import br.com.alura.screenmach.model.Episodio;
 import br.com.alura.screenmach.service.ConsumoAPI;
 import br.com.alura.screenmach.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -33,5 +36,59 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 10 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> e.avaliacao().equalsIgnoreCase("N/A"))
+                .peek(e -> System.out.println("Primeiro filtro(N/A)" + e))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .peek(e -> System.out.println("Ordenação " + e))
+                .limit(10)
+                .peek(e -> System.out.println("Limite " + e))
+                .map(e -> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Mapeamento" + e))
+                .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("Digite um trecho do título do eposódio: ");
+
+        var trechoTitulo = leitura.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if (episodioBuscado.isPresent()) {
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+        } else {
+            System.out.println("Episódio não encontrado!");
+        }
+
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextLine();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(Integer.parseInt(ano), 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 }
